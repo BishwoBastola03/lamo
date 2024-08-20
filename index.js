e44c2c75420df767cabbdd8aef63ce4f633d49d3
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-08-20 11:22:26 +0545

    Update index.js

--- index.js
+++ index.js
@@ -36,37 +36,3 @@ function startProject() {
 }
 
 startProject();
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const axios = require('axios');
-
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'temp', 'chatbot.html'));
-});
-
-const { RsnChat } = require('rsnchat');
-
-const rsnchat = new RsnChat('rsnai_C5Y6ZSoUt3LRAWopF6PQ2Uef');
-
-app.get('/architecture', async (req, res) => {
-		const query = req.query.ask;
-		if (!query) {
-				return res.status(400).json({ error: 'Your question is missing.' });
-		}
-
-		try {
-				const response = await rsnchat.gpt(query);
-				const jsonResponse = { architecture: response.message };
-				res.json(jsonResponse);
-		} catch (error) {
-				res.status(500).json({ error: 'An error occurred: ' + error.message });
-		}
-});
-
-app.listen(port, () => {
-		console.log(`Server is running on http://localhost:${port}`);
-});
