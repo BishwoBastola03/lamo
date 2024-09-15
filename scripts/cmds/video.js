eaac9fe2344877501b32f0feeb0b548502e03375
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-09-15 18:01:30 +0545

    Update video.js

--- scripts/cmds/video.js
+++ scripts/cmds/video.js
@@ -1,3 +1,4 @@
+const { GoatWrapper } = require('fca-liane-utils');
 const axios = require("axios");
 const fs = require('fs-extra');
 const path = require('path');
@@ -116,3 +117,5 @@ module.exports = {
         return video(api, event, args, message);
     }
 };
+const wrapper = new GoatWrapper(module.exports);
+wrapper.applyNoPrefix({ allowPrefix: true });
